6d2e33d5ddbcafab198b2658b443a83d67b76ff1
Wang Han <416810799@qq.com>
2020-05-26 20:58:17 +0800

    msm8974-common: Switch to TimeKeep
     * SE policies are imported and modified from
       https://github.com/sonyxperiadev/device-sony-sepolicy.
       Modifications are needed because qcom legacy policy
       contains conflicting labels and rules.
    
    Change-Id: Id04a824dea69976f6fc9d48bef77859cc82971ed

--- msm8974.mk
+++ msm8974.mk
@@ -246,6 +246,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     KeyHandler
 
+# TimeKeep
+PRODUCT_PACKAGES += \
+    timekeep \
+    TimeKeep
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.bacon
